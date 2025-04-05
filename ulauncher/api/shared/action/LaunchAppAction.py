89859cba1529d4c03f2ca42aaa35d373f4f834c2
Garry Hill <garry@magnetised.net>
2025-04-05 13:55:42 +0100

    use uwsm-app to launch things

--- ulauncher/api/shared/action/LaunchAppAction.py
+++ ulauncher/api/shared/action/LaunchAppAction.py
@@ -17,7 +17,7 @@ from ulauncher.api.shared.action.BaseAction import BaseAction
 logger = logging.getLogger(__name__)
 settings = Settings.get_instance()
 has_systemd = which("systemctl") and which("systemd-run")
-runs_in_systemd = has_systemd and os.system('systemctl --user is-active --quiet ulauncher') == 0
+runs_in_systemd = has_systemd and os.system("systemctl --user is-active --quiet ulauncher") == 0
 
 
 class LaunchAppAction(BaseAction):
@@ -36,32 +36,33 @@ class LaunchAppAction(BaseAction):
     def run(self):
         app = read_desktop_file(self.filename)
         app_id = Path(app.get_id()).stem
-        app_exec = app.get_string('Exec')
-        if app.get_boolean('DBusActivatable'):
+        app_exec = app.get_string("Exec")
+        if app.get_boolean("DBusActivatable"):
             # https://wiki.gnome.org/HowDoI/DBusApplicationLaunching
-            exec = ['gapplication', 'launch', app_id]
+            exec = ["gapplication", "launch", app_id]
         elif app_exec:
             # strip field codes %f, %F, %u, %U, etc
-            stripped_app_exec = re.sub(r'\%[uUfFdDnNickvm]', '', app_exec).rstrip()
-            if app.get_boolean('Terminal'):
-                terminal_exec = settings.get_property('terminal-command')
+            stripped_app_exec = re.sub(r"\%[uUfFdDnNickvm]", "", app_exec).rstrip()
+            if app.get_boolean("Terminal"):
+                terminal_exec = settings.get_property("terminal-command")
                 if terminal_exec:
-                    logger.info('Will run command in preferred terminal (%s)', terminal_exec)
+                    logger.info("Will run command in preferred terminal (%s)", terminal_exec)
                     exec = shlex.split(terminal_exec) + [stripped_app_exec]
                 else:
-                    exec = ['gtk-launch', app_id]
+                    exec = ["gtk-launch", app_id]
             else:
                 exec = shlex.split(stripped_app_exec)
 
         if not exec:
             logger.error("No command to run %s", app_id)
         else:
-            if runs_in_systemd and not app.get_boolean('X-Ulauncher-Inherit-Scope'):
+            if runs_in_systemd and not app.get_boolean("X-Ulauncher-Inherit-Scope"):
                 logger.warning("Will attempt to launch the app using systemd-run with --scope argument")
                 logger.warning("This prevents the apps from terminating if Ulauncher crashes or is restarted.")
                 logger.warning("On some systems with outdated systemd or incorrect permissions this doesn't work.")
                 logger.warning("If this happens to you, don't run Ulauncher from systemd.")
-                exec = ['systemd-run', '--user', '--scope'] + exec
+                # exec = ['systemd-run', '--user', '--scope'] + exec
+                exec = ["uwsm-app", *exec]
 
             env = dict(os.environ.items())
             # Make sure GDK apps aren't forced to use x11 on wayland due to ulauncher's need to run
@@ -69,14 +70,14 @@ class LaunchAppAction(BaseAction):
             env.pop("GDK_BACKEND", None)
 
             try:
-                logger.info('Run application %s (%s) Exec %s', app.get_name(), self.filename, exec)
+                logger.info("Run application %s (%s) Exec %s", app.get_name(), self.filename, exec)
                 envp = ["{}={}".format(k, v) for k, v in env.items()]
                 GLib.spawn_async(
                     argv=exec,
                     envp=envp,
                     flags=GLib.SpawnFlags.SEARCH_PATH_FROM_ENVP | GLib.SpawnFlags.SEARCH_PATH,
                     # setsid is really only needed if systemd-run is missing, but doesn't hurt to have.
-                    child_setup=os.setsid
+                    child_setup=os.setsid,
                 )
             except Exception as e:
-                logger.error('%s: %s', type(e).__name__, e)
+                logger.error("%s: %s", type(e).__name__, e)
