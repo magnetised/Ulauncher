8f78a79e2063bf8ab2e06b6d87e62c6d30b8d0ee
Garry Hill <garry@magnetised.net>
2025-04-05 14:26:08 +0100

    replace version placeholder with no formatting changes

--- setup.py
+++ setup.py
@@ -174,7 +174,7 @@ def main():
 
     DistUtilsExtra.auto.setup(
         name='ulauncher',
-        version='%VERSION%',
+        version='5.15.7-uwsm',
         license='GPL-3',
         author='Aleksandr Gornostal',
         author_email='ulauncher.app@gmail.com',
